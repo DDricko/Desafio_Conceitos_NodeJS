1b9344a6b5b13e838e7f928cdf699bc23347cae9
Rodrigo Nascimento <rodrigowaltmann.n@gmail.com>
2023-02-01 11:42:15 -0300

    Especificação dos testes - verificação de um todo não existente

--- src/index.js
+++ src/index.js
@@ -85,10 +85,14 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   const { user } = request;
-  const { title, deadline } = request.body;
   const { id } = request.params;
 
   const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: 'Todo not found'});
+  }
+  
   todo.done = true;
 
   return response.json(todo);
